738feb8b2f6390fbe99a75b34efebe3452c38634
iana-davydenko <iana.lady1302@gmail.com>
2020-09-22 18:49:50 +0300

    Initial commit

---
+++ for'o'.py
@@ -0,0 +1,7 @@
+def my_function(food):
+  for x in food:
+    print('the quantity of the letter a:', food.count('a'))
+    break
+
+my_function('apple banana cherry')
+my_function('woaw it is amazing')
---
+++ for1.py
@@ -0,0 +1,5 @@
+def function(line):
+    print('The quantity of the letters', line.count('o'))
+
+function('wow wow wow')
+function('count count mode')
